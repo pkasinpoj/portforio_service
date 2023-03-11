db17d196e871326a44deabc223806f4cc2b8e3bb
kasinpoj <59050167@kmitl.ac.th>
2023-03-11 13:20:19 -0800

    update allow origin

--- server.js
+++ server.js
@@ -11,7 +11,10 @@ app.use(express.json());
 app.use(bodyParser.urlencoded({ extended: false }));
 // app.use(cors());
 app.use(function (req, res, next) {
-  res.setHeader("Access-Control-Allow-Origin", "*");
+  res.setHeader(
+    "Access-Control-Allow-Origin",
+    "https://portfolioview-colon007march.vercel.app/"
+  );
   res.setHeader(
     "Access-Control-Allow-Methods",
     "GET, POST, OPTIONS, PUT, PATCH, DELETE"
