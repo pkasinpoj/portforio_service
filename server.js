4597a69749dafcb14a5c2b752eadccf148f1b385
kasinpoj <59050167@kmitl.ac.th>
2023-03-11 12:51:58 -0800

    update allow origin

--- server.js
+++ server.js
@@ -10,6 +10,19 @@ const app = express();
 app.use(express.json());
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(cors());
+app.use(function (req, res, next) {
+  res.setHeader("Access-Control-Allow-Origin", "*");
+  res.setHeader(
+    "Access-Control-Allow-Methods",
+    "GET, POST, OPTIONS, PUT, PATCH, DELETE"
+  );
+  res.setHeader(
+    "Access-Control-Allow-Headers",
+    "Origin, X-Requested-With, Content-Type, Accept,accesstoken"
+  );
+  res.setHeader("Access-Control-Allow-Credentials", true);
+  next();
+});
 // app.get("/", (req, res) => {
 //   res.send("Access denied!");
 // });
